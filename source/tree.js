e8dad0913239de79d7dc224fe0399ceb4601f4ab
NellinLin <ADshishova@mail.ru>
2019-09-03 01:56:22 +0300

    JSDoc, drawLine function

--- source/tree.js
+++ source/tree.js
@@ -1,24 +1,31 @@
 'use strict';
 
+/**
+ * Рисует ёлочку из '*'
+ * 
+ * @param {Number} lineCount - передаваемое целое число (высота ёлочки)
+ * @returns {String} - возвращаемая строка (изображение ёлочки)
+ * @example
+ * 
+ * tree(3);
+ * // => ' * \n***\n | \n'
+ */
 function tree(lineCount) {
-    if (lineCount < 3 || isNaN(+lineCount)) {
+    const count = +lineCount;
+    
+    if (count < 3 || isNaN(count)) {
         return null;
     }
-    const treeLines = lineCount - 1;
+
+    const treeLines = count - 1;
     let treePicture = '';
+    const drawLine = (str, padding) => str.padStart(str.length + padding).padEnd(str.length + padding * 2);
     
     for (let i = 0; i < treeLines; i++) {
-        treePicture +=
-            ' '.repeat(treeLines - i - 1) +
-            '*'.repeat(i * 2 + 1) +
-            ' '.repeat(treeLines - i - 1) +
-            '\n';
+        treePicture += drawLine('*'.repeat(i * 2 + 1), treeLines - i - 1) + '\n';
     }
-    
-    treePicture +=
-        ' '.repeat(treeLines - 1 ) +
-        '|' + ' '.repeat(treeLines - 1) +
-        '\n';
+
+    treePicture += drawLine('|', treeLines - 1 ) + '\n';
     
     return treePicture;
 }
